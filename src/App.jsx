6a7c50aed37c4bdaeb9e37e30e27a00a13b0b075
.rammex <104463176+rammex666@users.noreply.github.com>
2025-04-29 21:19:16 +0200

    BIG CHANGE

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,16 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Section from './components/Section';
-import Cards from './components/Cards';
-import PricingTable from './components/PricingTable';
-import Statistics from './components/Statistics';
-import Footer from './components/Footer';
-import './styles/App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-    return (
-        <div>
-            <div class="particles">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            <Navbar />
-            <Hero />
-            <Section id="project" title="Présentation du projet" content="Découvrez comment AnalyseYourDiscord transforme vos données Discord en informations claires et utiles." />
-            <Section id="features" title="Fonctionnalités principales">
-                <Cards />
-            </Section>
-            <Section id="price" title="Nos Offres">
-                <PricingTable />
-            </Section>
-            <Section id="statistics" title="Statistiques en direct">
-                <Statistics />
-            </Section>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="App">
+      <div className="background"></div>
+      <div className="overlay"></div>
+      <div className="content">
+        <h1>Analyse Your Discord</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
