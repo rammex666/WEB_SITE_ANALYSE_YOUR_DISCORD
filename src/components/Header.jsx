0f9774a274b05c975601dea495e6ed4cd79f438e
.rammex <romain.kaspar@gmail.com>
2025-04-30 15:54:18 +0200

    navbar logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../style/Header.css";
+import logo from "../assets/logo.png"
 
 function Header() {
   return (
     <header className="header">
-        <img src="../assets/logoayd.png" className="logoayd"/>
+        <img src={logo} className="logoayd"/>
         <h1 className="header-title">Welcome to Analyse Your Discord</h1>
     </header>
   );
